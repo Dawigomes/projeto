a3a9634fae306237466ac0fec48e7e13d76fffb3
Dawi Gomes <dawigomes@hotmail.com>
2023-11-15 16:49:25 -0300

    add new image light

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function togglemode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatarlight.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem ligh mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
